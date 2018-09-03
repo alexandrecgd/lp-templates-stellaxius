1b8b951b84993682b2d0d0975260c1d73f308823
AlexandreCGD <alexandrecgduarte@gmail.com>
2018-09-03 13:58:58 +0100

    Correções

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	"use strict";
 
@@ -10,7 +9,7 @@ $(document).ready(function(){
 
 
 	$(".fullscreen").css("height", window_height)
-	$(".fitscreen").css("height", fitscreen);
+	$(".fitscreen").css("height", fitscreen - 150);
 
   //-------- Active Sticky Js ----------//
      $(".default-header").sticky({topSpacing:0});
@@ -20,7 +19,7 @@ $(document).ready(function(){
      $('select').niceSelect();
      
      
-   // -------   Active Mobile Menu-----//
+  // -------   Active Mobile Menu-----//
 
   $(".menu-bar").on('click', function(e){
       e.preventDefault();
@@ -70,38 +69,6 @@ $(document).ready(function(){
         }
       }
     });
-
-      // -------   Mail Send ajax
-
-         $(document).ready(function() {
-            var form = $('#myForm'); // contact form
-            var submit = $('.submit-btn'); // submit button
-            var alert = $('.alert-msg'); // alert div for show alert message
-
-            // form submit event
-            form.on('submit', function(e) {
-                e.preventDefault(); // prevent default form submit
-
-                $.ajax({
-                    url: 'mail.php', // form action url
-                    type: 'POST', // form submit method get/post
-                    dataType: 'html', // request type html/json/xml
-                    data: form.serialize(), // serialize form data
-                    beforeSend: function() {
-                        alert.fadeOut();
-                        submit.html('Sending....'); // change submit button text
-                    },
-                    success: function(data) {
-                        alert.html(data).fadeIn(); // fade in response data
-                        form.trigger('reset'); // reset form
-                        submit.attr("style", "display: none !important");; // reset submit button text
-                    },
-                    error: function(e) {
-                        console.log(e)
-                    }
-                });
-            });
-        });
 
 
  });
